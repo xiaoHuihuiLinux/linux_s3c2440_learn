2a70e88327126e9862a34bbf4f64899f8114ef7f
linux <1451414317@qq.com>
2020-05-02 00:09:43 +0800

    怎加线程锁以及条件变量的学习以及代码的注释

--- linuxSystemProgrome/code/thread_pool/threadpool.c
+++ linuxSystemProgrome/code/thread_pool/threadpool.c
@@ -6,6 +6,7 @@
 	if(list != NULL) list->prev = item;						\
 	list = item;/*item成为list的新的头地址*/ 						\
 }while(0)
+
 /*删除*/
 #define LL_REMOVE(item,list) do{ 							\
 	if(item->prev != NULL)  item->prev->next = item->next;  \
@@ -13,6 +14,7 @@
 	if(list == item) list = item->next; \/*删除的item是头节点情况*/
 	item->prev = item->next = NULL;							\
 }while(0)
+
 /*任务队列
 每一个NJOB就相当于任务队列中的每个任务
 */
@@ -26,36 +28,39 @@ struct NJOB{
 	
 /*执行队列，相当于线程池中的一个个线程，每个线程都去操作任务队列的相当于操作临界资源*/
 typedef struct NWORKER {
-	pthread_t id;/*线程id*/
+	pthread_t id;/*线程id pthread_t数据类型表示线程id*/
 	int terminate;//状态牌决定这个线程是否工作
-	struct NMANAGER* pool;//引入pool
+	struct NMANAGER* pool;//引入哪个pool
 	struct NWORKER *prev;
 	struct NWORKER *next;
 }NWORKER,*p_NWORKER;
+
 /*管理线程与任务*/
 typedef struct NMANAGER {
 	/*对临界资源的处理 加锁*/ 
-	pthread_mutex_t mtx;
+	pthread_mutex_t mtx;//pthread_mutex_t 锁的数据类型
 	/*当任务队列为空的条件下，线程就会休息（挂起）。
+	为什莫要用到条件变量是因为假如线程得到锁后区操作队列发现任务队列为空则会阻塞等待，
+	则任务队列就不会被填充为有效，造成死锁。所以要用条件变量发现不满足执行的条件之后
+	释放锁，等待在条件变量上
 	条件变量的用法：当条件变量满足（任务队列不为空）线程就去操作队列*/
-	pthread_cond_t cond;
+	pthread_cond_t cond;//pthread_cond_t 条件变量数据类型
 	struct NJOB  * jobs;
 	struct NWORKER *workes;
 }nThreadPool,*P_nThreadPool;
-//封装为sdk
 
-//
+//封装为sdk
 void *thread_cb(void *arg)//线程的回调 传woker参数进来
 {
 	struct NWORKER* worker = (struct NWORKER*)arg;
 	while(1)
 	{	
 		pthread_mutex_lock(&worker->pool->mtx);//加锁
-		while(worker->pool->jobs == NULL)//任务队列为空
+		while(worker->pool->jobs == NULL)//任务队列为空 也就是条件为假
 		{
 			if(worker->terminate == 0) break;//等于1退出
 			
-			pthread_cond_wait(&worker->pool->cond,&worker->pool->mtx);
+			pthread_cond_wait(&worker->pool->cond,&worker->pool->mtx);// 令进程等待在条件变量上
 		}
 		/*相当于从银行等待的客户中叫来一个人，然后办理他的具体业务*/
 		//1 从等待的队列中拿出数据
@@ -78,9 +83,11 @@ int nThreadPoolCreate(nThreadPool *pool,int numberworks)//线程个数numberworks
 	if(pool == NULL) return -1;//失败
 	memset(pool,0,sizeof(nThreadPool));
 	/*对结构体或者一块数据初始化赋值可以用memcpy*/
-	pthread_mutex_t blank_mutex = PTHREAD_MUTEX_INITTALTZER;//pthread_mutex_t 应该是锁的数据类型
+	//初始化线程锁pthread_mutex_init(&mtx, NULL);同样也可以采取下面的方法
+	pthread_mutex_t blank_mutex = PTHREAD_MUTEX_INITIALIZER;//pthread_mutex_t 应该是锁的数据类型
 	memcpy(pool->mtx,&blank_mutex,sizeof(pthred_mutex_t));
-	pthread_cond_t blank_cond = PTHREAD_COND_INITTALTZER;//pthread_cond_t 应该是条件变量的数据类型
+	
+	pthread_cond_t blank_cond = PTHREAD_COND_INITIALIZER;//pthread_cond_t 应该是条件变量的数据类型
 	memcpy(pool->cond,&blank_cond,sizeof(pthread_cond_t));
 	//works
 	for(i= 0; i < numberworks;i++)
@@ -105,9 +112,9 @@ int nThreadPoolDestory(nThreadPool *pool)
 /*往任务队列仍一个任务（也就是往线程池仍任务）要发送一个信号通知线程，因为线程都要满足条件等待*/
 int nThreadPoolPushTask(nThreadPool * pool,struct NJOB*job)
 {/*多线程的增加删除队列都要加锁*/
-	pthread_mutex_lock(&pool->mtx);//加锁
+	pthread_mutex_lock(&pool->mtx);//加锁这个操作是阻塞调用的，也就是说，如果这个锁此时正在被其它线程占用， 那么 pthread_mutex_lock() 调用会进入到这个锁的排队队列中
 	LL_ADD(job, pool->jobs);
-	pthread_cond_signal(pool->cond);
+	pthread_cond_signal(pool->cond);// 通知等待在条件变量上的线程
 	pthread_mutex_unlock(&pool->mtx);//加锁
 }
 #if 1
